3e0cd4bc4085ca3dce860f5a9ff9bf44625f82ea
Andres Romero <andbrew.dev@proton.me>
2023-01-03 21:54:43 -0600

    feat: implement Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,25 @@
 export const Hero = () => {
   return (
-    <div className="hero min-h-screen">
+    <div className="hero min-h-screen bg-black text-white">
       <div className="hero-content flex-col lg:flex-row">
         <img
-          src="https://placeimg.com/260/400/arch"
+          src="assets/andbrew.jpg"
           className="max-w-sm rounded-lg shadow-2xl"
         />
         <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <h1 className="text-8xl font-bold">I'm AndBrew</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Welcome to my portfolio website! My name is Andres Romero and I am a
+            recent graduate in Computer Science from Universidad Autónoma de
+            Chihuahua.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <p>
+            Throughout my studies, I have developed a strong foundation in
+            computer science concepts and skills, including imperative
+            programming, algorithm analysis, database design, linux basics,
+            version control, web development, container leveraging, and machine
+            learning engineering.
+          </p>
         </div>
       </div>
     </div>
