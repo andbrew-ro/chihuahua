5098c233c9d7f03a69c1f9c714c70928a280b169
Andres Romero <andbrew.dev@proton.me>
2023-01-06 22:51:28 -0600

    you just wait sunshine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { AboutMe } from "./components/AboutMe";
 import { Hero } from "./components/Hero";
+import { Projects } from "./components/Projects";
 
 export const App = () => {
     return (
         <div>
             <Hero/>
+            <AboutMe/>
+            <Projects/>
         </div>
     );
 };
